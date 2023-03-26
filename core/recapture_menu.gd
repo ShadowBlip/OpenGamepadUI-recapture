dbd3672021ed48884cbea18c8b4450176c423778
William Edwards <shadowapex@gmail.com>
2023-03-25 17:23:26 -0700

    Implement recapture

---
+++ core/recapture_menu.gd
@@ -0,0 +1,72 @@
+extends Control
+
+const NotificationManager := preload("res://core/global/notification_manager.tres")
+const icon := preload("res://plugins/recapture/assets/record-rec.svg")
+
+@onready var rec_button := $%RecButton
+@onready var focus_node := $%RecButton
+@onready var timer := $Timer
+
+var plugins_dir := ProjectSettings.globalize_path(PluginLoader.PLUGINS_DIR)
+var deps_dir := "/".join([plugins_dir, "recapture", "assets", "deps"])
+var recapture_bin := "/".join([deps_dir, "recapture"])
+var gst_plugin_path := "/".join([deps_dir, "plugins"])
+var ld_library_path := "/".join([deps_dir, "lib"])
+
+var pid := -1
+var recording := false
+var logger := Log.get_logger("Recapture")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	rec_button.pressed.connect(_on_button_pressed)
+	timer.timeout.connect(_check_recording)
+
+
+func _on_button_pressed() -> void:
+	if recording:
+		_stop_recording()
+		return
+	_start_recording()
+
+
+func _start_recording() -> void:
+	# Build the command to run
+	var cmd := "env"
+	var args := PackedStringArray()
+	var env_vars := ["GST_VAAPI_ALL_DRIVERS=1", "GST_PLUGIN_PATH="+gst_plugin_path, "LD_LIBRARY_PATH="+ld_library_path]
+	args.append_array(env_vars)
+	args.append(recapture_bin)
+	args.append("record")
+	logger.info("Starting capture command: " + cmd + " " + str(args))
+	pid = OS.create_process(cmd, args)
+	if pid < 1:
+		var notify := Notification.new("Unable to start capture")
+		notify.icon = icon
+		NotificationManager.show(notify)
+		return
+	
+	# Update the button text
+	rec_button.text = "Stop recording"
+	recording = true
+	timer.start()
+	
+	
+func _stop_recording() -> void:
+	timer.stop()
+	# Update the button text
+	rec_button.text = "Start recording"
+	recording = false
+	if pid > 0:
+		OS.kill(pid)
+
+
+func _check_recording() -> void:
+	if OS.is_process_running(pid):
+		return
+	logger.warn("Recording process is not running anymore")
+	var notify := Notification.new("Recording has stopped")
+	notify.icon = icon
+	NotificationManager.show(notify)
+	_stop_recording()
--- plugin.gd
+++ plugin.gd
@@ -1,6 +1,11 @@
 extends Plugin
 
+const recapture_scene := preload("res://plugins/recapture/core/recapture_menu.tscn")
+const icon := preload("res://plugins/recapture/assets/record-rec.svg")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	logger = Log.get_logger("TemplatePlugin", Log.LEVEL.DEBUG)
-	logger.info("Template plugin loaded")
+	logger = Log.get_logger("Recapture", Log.LEVEL.DEBUG)
+	logger.info("Recapture plugin loaded")
+	var recapture_menu := recapture_scene.instantiate()
+	add_to_qam.call_deferred(recapture_menu, icon, recapture_menu.focus_node)
